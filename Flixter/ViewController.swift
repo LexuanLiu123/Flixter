9e127f780d27944a662efa996992263ae7e1ef78
LexuanLiu123 <lel006@ucsd.edu>
2020-02-01 13:33:01 -0800

    Initial Commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Judy Liu on 2020-01-30.
+//  Copyright © 2020 Judy Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
