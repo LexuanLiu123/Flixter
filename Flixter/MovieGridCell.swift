e6b416fd6f65b15cc5c73737eb5720b63cfc752b
LexuanLiu123 <lel006@ucsd.edu>
2020-02-07 20:57:17 -0800

    Flix Part 2 1. added table detail view 2. added superhero collection view

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Judy Liu on 2020-02-07.
+//  Copyright © 2020 Judy Liu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
---
+++ Flixter/MovieGridViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridViewCell.swift
+//  Flixter
+//
+//  Created by Judy Liu on 2020-02-07.
+//  Copyright © 2020 Judy Liu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
